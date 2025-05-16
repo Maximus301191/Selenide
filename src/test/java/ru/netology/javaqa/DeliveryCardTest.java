d7a8c7b0b679b62151d113eb0d0b4380eadd9689
Maximus301191 <maxalut301191@gmail.com>
2025-05-16 22:17:40 +0300

    Finally commit

--- src/test/java/ru/netology/javaqa/DeliveryCardTest.java
+++ src/test/java/ru/netology/javaqa/DeliveryCardTest.java
@@ -1,6 +1,7 @@
 package ru.netology.javaqa;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,10 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class DeliveryCardTest {
 
@@ -37,5 +40,38 @@ public class DeliveryCardTest {
         $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(visible);
 
 
+    }
+
+    @Test
+    void shouldDeliverCardByListsForSevenDays() {
+
+        long addDays = 7;
+        String planningDay = generateDate(addDays, "d");
+        String planningDate = generateDate(addDays, "dd.MM.yyyy");
+
+        Selenide.open("http://localhost:9999");
+        SelenideElement formElement = $("form");
+
+        formElement.$("[data-test-id='city'] input").setValue("Ма");
+        $$(".menu .menu-item__control").find(exactText("Майкоп")).click();
+        formElement.$(".icon").click();
+
+        LocalDate planningDateL = LocalDate.now().plusDays(addDays);
+        LocalDate defaultDate = LocalDate.now().plusDays(4);
+
+        ElementsCollection arrow = $$(".calendar__arrow_direction_right");
+        int monthDiff = (planningDateL.getYear() - defaultDate.getYear()) * 12 + (planningDateL.getMonthValue() - defaultDate.getMonthValue());
+        while (monthDiff > 0) {
+            $(arrow.get(1)).click();
+            monthDiff = monthDiff-1;
+        }
+        $$(".calendar__day").find(exactText(planningDay)).click();
+
+        formElement.$("[data-test-id='name'] input").setValue("Петров Иван");
+        formElement.$("[data-test-id='phone'] input").setValue("+79119638521");
+        formElement.$("[data-test-id='agreement']").click();
+        formElement.$("[class='button__content']").click();
+        $("[data-test-id=notification].notification_visible .notification__title").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Успешно!"));
+        $("[data-test-id=notification].notification_visible .notification__content").shouldBe(visible).shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
